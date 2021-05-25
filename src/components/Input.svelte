a41e0bb2f80516c1767c43a14369617be1933ac8
Pedro Henrique <pedrovida770@gmail.com>
2021-05-25 20:01:58 -0300

    Mostrar TODO'S na tela

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,28 +1,75 @@
+<script>
+    const todos = [
+        {
+            nome: "Fazer Compras",
+            concluido: false
+        },
+        {
+            nome: "Ir ao Dentista",
+            concluido: true
+        },
+    ]
+</script>
+
 <style>
-    input {
+    .add-todo {
         padding: 10px 34px;
         font-family: "Open Sans";
         font-size: 20px;
         border: 0;
-        box-shadow: inset 0 -2px 1px rgb(0 0 0 / 3%);
         outline: 0;
         max-width: 400px;
         width: 100%;
     }
 
-    input::placeholder {
+    .add-todo::placeholder {
         color: rgba(0, 0, 0, 0.3);
         font-style: italic;
     }
 
-    div {
+    section {
         box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%), 0 25px 50px 0 rgb(0 0 0 / 10%);
         margin-bottom: 20px;
     }
 
+    .todo {
+        font-weight: 100;
+        padding: 12px;
+        display: flex;
+        align-items: center;
+        border: 1px solid #ededed;
+    }
+
+    .todo p {
+        margin: 0 0 0 6px;
+        color: #4d4d4d;
+    }
+
+    .todo input {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        appearance: none;
+        height: 24px;
+        width: 24px;
+        border-radius: 50%;
+        border: 1px solid rgba(175, 47, 47, 0.4);
+    }
+
+    .todo input:checked::after {
+        content: '\2714';
+        color: rgba(175, 47, 47, 0.4);
+    }
+
 </style>
 
-<div>
-    <input type="text" placeholder="O que precisa ser feito?">
-</div>
+<section>
+    <input class="add-todo" type="text" placeholder="O que precisa ser feito?">
+    {#each todos as todo}
+        <div class="todo">
+            <input type="checkbox">
+            <p>{todo.nome}</p>
+        </div>
+    {/each}
+</section>
 
